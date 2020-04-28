895cf545ab1cd0cf253fee95dade640c786dfdfb
wdpfm <pfm9866@126.com>
2020-04-28 09:06:21 +0800

    Initial commit

---
+++ app/src/main/java/com/wdpfm/myviewpager/MainActivity.java
@@ -0,0 +1,30 @@
+package com.wdpfm.myviewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager myvpager;
+    private ArrayList<View> aList;
+    private MyPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myvpager=findViewById(R.id.viewPager);
+        aList=new ArrayList<View>();
+        LayoutInflater li=getLayoutInflater();
+        aList.add(li.inflate(R.layout.view_one,null,false));
+        aList.add(li.inflate(R.layout.view_two,null,false));
+        aList.add(li.inflate(R.layout.view_threee,null,false));
+        mAdapter=new MyPagerAdapter(aList);
+        myvpager.setAdapter(mAdapter);
+    }
+}
